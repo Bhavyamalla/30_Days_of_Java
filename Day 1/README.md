18fe2f086ae4b81be8b0d7ad41e726335540ad0d
BhavyaMalla <bhavyamalla9@gmail.com>
2026-06-01 22:38:49 +0530

    Day 01: Hello World and Input

---
+++ Day 1/README.md
@@ -0,0 +1,16 @@
+# Day 1 - Hello World and Input
+
+## Description
+Created a Java program that:
+- Prints "Hello, World."
+- Accepts a string input from the user
+- Prints the entered string
+
+## Concepts Practiced
+- Scanner Class
+- User Input
+- String Variables
+- Output Statements
+
+## Status
+Completed ✅
---
+++ Day 1/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+
+    public static void main(String[] args) {
+        
+        Scanner obj = new Scanner(System.in);
+
+        String input = obj.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(input);
+    }
+}
